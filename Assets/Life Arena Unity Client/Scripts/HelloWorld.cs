34d4d8ac152838b27c52dbb75fac0aef5c43b1dd
Avangardum <yurykuz65@gmail.com>
2023-04-19 13:25:28 +0300

    Initial commit

---
+++ Assets/Life Arena Unity Client/Scripts/HelloWorld.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Avangardum.LifeArena.UnityClient
+{
+    public class HelloWorld : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hello, world!!!");
+        }
+    }
+}
