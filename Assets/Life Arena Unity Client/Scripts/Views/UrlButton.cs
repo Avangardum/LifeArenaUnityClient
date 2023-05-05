0bc99981436129380e05960de2ddb47f206c2ce1
Avangardum <yurykuz65@gmail.com>
2023-05-05 14:29:47 +0300

    Added url header buttons

---
+++ Assets/Life Arena Unity Client/Scripts/Views/UrlButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Avangardum.LifeArena.UnityClient.Views
+{
+    [RequireComponent(typeof(Button))]
+    public class UrlButton : MonoBehaviour
+    {
+        [SerializeField] private string _url;
+
+        private Button _button;
+        
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            
+            _button.onClick.AddListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            Application.OpenURL(_url);
+        }
+    }
+}
